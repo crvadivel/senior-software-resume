9ce4779a77fc8abb0efb9c0c024fbb5322f23320
Duraisamy <preethi.duraisamy@hpe.com>
2018-02-14 22:50:19 +0530

    senior-software-resume

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { MaincontentComponent } from './maincontent/maincontent.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    MaincontentComponent
   ],
   imports: [
     BrowserModule
